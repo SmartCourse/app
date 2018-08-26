bdb06ebebe1696584792db6dc2df4e774355d5f8
Alex Hinds <alex.hinds141@gmail.com>
2018-08-26 14:49:09 +1000

    Feed requirements 1/2 (#95)
    * added some super basic styling
    
    * minor changes to tests, expected api structure
    
    * added route between title and question page

---
+++ frontend/src/components/Feed.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="Feed">
+    <h1>{{ title }}</h1>
+    <section class="questions">
+      <ol>
+        <li :key="question.id" v-for="question in questions">
+           <QuestionCard :question="question"/>
+        </li>
+      </ol>
+    </section>
+  </div>
+</template>
+
+<script>
+import QuestionCard from './QuestionCard'
+
+export default {
+  name: 'Feed',
+  components: {
+    QuestionCard
+  },
+  props: {
+    title: String,
+    questions: Array
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+li {
+  list-style: none;
+}
+
+</style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -5,6 +5,7 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -18,6 +19,14 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/question/:id',
+      name: 'question',
+      props: ({params: { id }}) => ({
+        id
+      }),
+      component: () => import(/* webpackChunkName: "about" */ './views/Question.vue')
     }
   ]
 })
