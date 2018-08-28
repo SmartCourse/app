fe18c9d5080e89e063690d0d0ace2ad03b559862
Alex Hinds <alex.hinds141@gmail.com>
2018-08-28 20:36:56 +1000

    fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,10 +11,8 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '../public')))
 
-const indexRouter = require('./controllers')
 const apiRouter = require('./controllers/api')
 
-app.use('/', indexRouter)
 app.use('/api', apiRouter)
 
 /* 
