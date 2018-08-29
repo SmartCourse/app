22e59dd0a3f08e05aaf202e0cfa385c1d01627e2
Alex Hinds <alex.hinds141@gmail.com>
2018-08-29 16:11:27 +1000

    Added a new backend linting script (#100)

--- backend/src/controllers/course.js
+++ backend/src/controllers/course.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = express.Router({mergeParams: true})
+const router = express.Router({ mergeParams: true })
 
 const questionRouter = require('./question')
 const reviewRouter = require('./review')
@@ -8,29 +8,26 @@ router.use('/question', questionRouter)
 router.use('/review', reviewRouter)
 
 /* Get the course data for a specific course id */
-router.get('/', function(req, res) {
-
-    //let cid = req.params.cid;
+router.get('/', function (req, res) {
+    // let cid = req.params.cid;
 
     res.send('<h1>Course Data</h1>')
 })
 
 /* Get page (N) questions for a course */
-router.get('/questions', function(req, res) {
+router.get('/questions', function (req, res) {
+    // let cid = req.params.cid;
+    // let page_id = req.query.p;
 
-  //let cid = req.params.cid;
-  //let page_id = req.query.p;
-
-  res.send('<h1>Page Questsions</h1>')
+    res.send('<h1>Page Questsions</h1>')
 })
 
 /* Get page (N) reviews for a course */
-router.get('/reviews', function(req, res) {
+router.get('/reviews', function (req, res) {
+    // let cid = req.params.cid;
+    // let page_id = req.query.p;
 
-  //let cid = req.params.cid;
-  //let page_id = req.query.p;
-
-  res.send('<h1>Page Reviews</h1>')
+    res.send('<h1>Page Reviews</h1>')
 })
 
 module.exports = router
