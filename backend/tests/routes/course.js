0ee5d74246c9bfdaef1a0b2843f25912fc5e317a
Nuno Das Neves <ndasneves@gmail.com>
2018-09-06 12:18:39 +1000

    Backend tests for answer route (#123)
    * improved route tests
    
    * removed controller tests, improved clarity & stability of route tests

--- backend/tests/routes/course.js
+++ backend/tests/routes/course.js
@@ -1,5 +1,7 @@
 const app = require('../../src')
 const supertest = require('supertest')(app)
+const chai = require('chai')
+const expect = chai.expect
 
 describe('Course route testing', () => {
     it('GET course index', () =>
@@ -10,13 +12,38 @@ describe('Course route testing', () => {
             .expect(200)
     )
 
-    it('GET /api/course/1/questions', () =>
-        supertest
-            .get('/api/course/1/questions')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200)
-    )
+    describe('GET /api/course/1/questions', () => {
+        let request
+
+        before(() => {
+            request = supertest
+                .get('/api/course/1/questions')
+                .set('Accept', 'application/json')
+                .expect(200)
+            return request
+        })
+
+        it('correct number of questions', () =>
+            request.then(({body}) =>
+                expect(body.length).is.a('number'))
+        )
+
+        it('question has a title', () =>
+            request.then(({body}) =>
+                expect(body[0].title).is.a('string'))
+        )
+
+        it('question has a body', () =>
+            request.then(({body}) =>
+                expect(body[0].body).is.a('string'))
+        )
+
+        it('question has a course id', () =>
+            request.then(({body}) =>
+                expect(body[0].courseID).is.a('number'))
+        )
+
+    })
 
     it('POST /api/course/1/question', () =>
         supertest
--- backend/tests/routes/questions.js
+++ backend/tests/routes/questions.js
@@ -1,36 +1,88 @@
 const app = require('../../src')
 const assert = require('assert')
 const supertest = require('supertest')(app)
+const chai = require('chai')
+const expect = chai.expect
 
-/* TODO build out tests to also include catching expected JSON response */
-describe('Question route testing', () => {
-    it('GET /api/question/1', () =>
-        supertest
-            .get('/api/question/1')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200)
-    )
+describe('Test question routes', () => {
+
+    describe('GET /api/question/1', () => {
+        let request
+
+        before(() => {
+            request = supertest
+                .get('/api/question/1')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+            return request
+        })
+
+        it('question has a title', () =>
+            request.then(({body}) =>
+                expect(body.title).is.a('string'))
+        )
+
+        it('question has a body', () =>
+            request.then(({body}) =>
+                expect(body.body).is.a('string'))
+        )
+
+        it('question has a course id', () =>
+            request.then(({body}) =>
+                expect(body.courseID).is.a('number'))
+        )
+
+    })
+
 })
 
-describe('Answer route testing', () => {
-    it('GET /api/question/1/answers', () =>
-        supertest
-            .get('/api/question/1/answers')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200)
-    )
+describe('Test answer routes', () => {
+    describe('GET /api/question/1/answers', () => {
+        let request
 
-    it('POST /api/question/1/answers', () =>
-        supertest
-            .post('/api/question/1/answers')
-            .send({ body: 'testu' })
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .then(response => {
-                assert(response.body.length === 2)
-            })
-    )
+        before(() => {
+            request = supertest
+                .get('/api/question/1/answers')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+            return request
+        })
+
+        it('has the right number of answers', () =>
+            request.then(({body}) =>
+                assert(body.length >= 1))
+        )
+
+        it('has a valid answer', () =>
+            request.then(({body}) =>
+                expect(body[0].body).to.be.a('string'))
+        )
+    })
+
+    describe('POST /api/question/1/answers', () => {
+        let request
+
+        before(() => {
+            request = supertest
+                .post('/api/question/1/answers')
+                .send({ body: 'superruuu____testu' })
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+            return request
+        })
+
+        it('has the right number of answers', () =>
+            request.then(({body}) =>
+                assert(body.length >= 1))
+        )
+
+        it('has the answer we POSTed', () =>
+            request.then(({body}) =>
+                expect(body.filter(ans => ans.body == 'superruuu____testu').length).to.equal(1))
+        )
+    })
+
 })
