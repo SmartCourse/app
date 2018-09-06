33a100c8f363ef198e3713ca2b173d408e16815a
Nuno Das Neves <ndasneves@gmail.com>
2018-09-06 12:15:25 +1000

    removed controller tests, improved clarity & stability of route tests

--- backend/tests/routes/questions.js
+++ backend/tests/routes/questions.js
@@ -4,79 +4,85 @@ const supertest = require('supertest')(app)
 const chai = require('chai')
 const expect = chai.expect
 
-describe('GET /api/question/1', () => {
-    let request
+describe('Test question routes', () => {
 
-    before(() => {
-        request = supertest
-            .get('/api/question/1')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200)
-        return request
+    describe('GET /api/question/1', () => {
+        let request
+
+        before(() => {
+            request = supertest
+                .get('/api/question/1')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+            return request
+        })
+
+        it('question has a title', () =>
+            request.then(({body}) =>
+                expect(body.title).is.a('string'))
+        )
+
+        it('question has a body', () =>
+            request.then(({body}) =>
+                expect(body.body).is.a('string'))
+        )
+
+        it('question has a course id', () =>
+            request.then(({body}) =>
+                expect(body.courseID).is.a('number'))
+        )
+
     })
-
-    it('question has a title', () =>
-        request.then(({body}) =>
-            expect(body.title).is.a('string'))
-    )
-
-    it('question has a body', () =>
-        request.then(({body}) =>
-            expect(body.body).is.a('string'))
-    )
-
-    it('question has a course id', () =>
-        request.then(({body}) =>
-            expect(body.courseID).is.a('number'))
-    )
 
 })
 
-describe('GET /api/question/1/answers', () => {
-    let request
+describe('Test answer routes', () => {
+    describe('GET /api/question/1/answers', () => {
+        let request
 
-    before(() => {
-        request = supertest
-            .get('/api/question/1/answers')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200)
-        return request
+        before(() => {
+            request = supertest
+                .get('/api/question/1/answers')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+            return request
+        })
+
+        it('has the right number of answers', () =>
+            request.then(({body}) =>
+                assert(body.length >= 1))
+        )
+
+        it('has a valid answer', () =>
+            request.then(({body}) =>
+                expect(body[0].body).to.be.a('string'))
+        )
     })
 
-    it('has the right number of answers', () =>
-        request.then(({body}) =>
-            expect(body.length).to.equal(1))
-    )
+    describe('POST /api/question/1/answers', () => {
+        let request
 
-    it('has a valid answer', () =>
-        request.then(({body}) =>
-            expect(body[0].body).to.be.a('string'))
-    )
-})
+        before(() => {
+            request = supertest
+                .post('/api/question/1/answers')
+                .send({ body: 'superruuu____testu' })
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+            return request
+        })
 
-describe('POST /api/question/1/answers', () => {
-    let request
+        it('has the right number of answers', () =>
+            request.then(({body}) =>
+                assert(body.length >= 1))
+        )
 
-    before(() => {
-        request = supertest
-            .post('/api/question/1/answers')
-            .send({ body: 'superruuu____testu' })
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200)
-        return request
+        it('has the answer we POSTed', () =>
+            request.then(({body}) =>
+                expect(body.filter(ans => ans.body == 'superruuu____testu').length).to.equal(1))
+        )
     })
 
-    it('has the right number of answers', () =>
-        request.then(({body}) =>
-            expect(body.length).to.equal(2))
-    )
-
-    it('has the answer we POSTed', () =>
-        request.then(({body}) =>
-            expect(body.filter(ans => ans.body == 'superruuu____testu').length).to.equal(1))
-    )
 })
-
