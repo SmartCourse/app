544f7ade86a52500e67d73f87ad5d06633489f79
Nuno Das Neves <ndasneves@gmail.com>
2018-09-06 11:18:26 +1000

    improved route tests

--- backend/tests/routes/questions.js
+++ backend/tests/routes/questions.js
@@ -1,37 +1,82 @@
 const app = require('../../src')
 const assert = require('assert')
 const supertest = require('supertest')(app)
+const chai = require('chai')
+const expect = chai.expect
 
-/* TODO build out tests to also include catching expected JSON response */
-describe('Question route testing', () => {
-    it('GET /api/question/1', () =>
-        supertest
+describe('GET /api/question/1', () => {
+    let request
+
+    before(() => {
+        request = supertest
             .get('/api/question/1')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
+        return request
+    })
+
+    it('question has a title', () =>
+        request.then(({body}) =>
+            expect(body.title).is.a('string'))
     )
+
+    it('question has a body', () =>
+        request.then(({body}) =>
+            expect(body.body).is.a('string'))
+    )
+
+    it('question has a course id', () =>
+        request.then(({body}) =>
+            expect(body.courseID).is.a('number'))
+    )
+
 })
 
-describe('Answer route testing', () => {
-    it('GET /api/question/1/answers', () =>
-        supertest
+describe('GET /api/question/1/answers', () => {
+    let request
+
+    before(() => {
+        request = supertest
             .get('/api/question/1/answers')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
+        return request
+    })
+
+    it('has the right number of answers', () =>
+        request.then(({body}) =>
+            expect(body.length).to.equal(1))
     )
 
-    it('POST /api/question/1/answers', () =>
-        supertest
+    it('has a valid answer', () =>
+        request.then(({body}) =>
+            expect(body[0].body).to.be.a('string'))
+    )
+})
+
+describe('POST /api/question/1/answers', () => {
+    let request
+
+    before(() => {
+        request = supertest
             .post('/api/question/1/answers')
             .send({ body: 'superruuu____testu' })
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
-            .then(response => {
-                assert(response.body.length === 2)
-                assert(response.body.filter(it => it.body == 'superruuu____testu').length == 1)
-            })
+        return request
+    })
+
+    it('has the right number of answers', () =>
+        request.then(({body}) =>
+            expect(body.length).to.equal(2))
+    )
+
+    it('has the answer we POSTed', () =>
+        request.then(({body}) =>
+            expect(body.filter(ans => ans.body == 'superruuu____testu').length).to.equal(1))
     )
 })
+
