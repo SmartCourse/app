7382e9597e530c0ead0f618add62aeca6f30e6f3
Alex Hinds <alex.hinds141@gmail.com>
2018-09-08 15:41:15 +1000

    appeased nuno

--- frontend/src/utils/api/errors.js
+++ frontend/src/utils/api/errors.js
@@ -1,10 +1,7 @@
 export default class APIError extends Error {
-  constructor(message, code) {
-    if (!code) {
-      super(`API Server Error 500: ${message}`)
-    } else {
-      super(`API Error ${code}: ${message}`)
-    }
+  constructor(message, code = 500) {
+    super(message)
+    this.code = code
     this.name = 'APIError'
     console.warn(this)
   }
