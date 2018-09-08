b4e2db89ec128ee4e7da8b8bcdca958a18bed949
Nuno Das Neves <ndasneves@gmail.com>
2018-09-08 13:30:24 +1000

    S4 frontend basic tests (#136)
    * answer card tests
    
    * basic answer form tests
    
    * deep equal for answer form emit test

---
+++ frontend/tests/unit/AnswerCard.spec.js
@@ -0,0 +1,31 @@
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import AnswerCard from '@/components/questions-answers/AnswerCard'
+
+describe('AnswerCard.vue', () => {
+  before(function () {
+    this.card = {
+      id: 1,
+      likes: 10,
+      published: Date.now(),
+      body:
+        'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+    }
+    this.wrapper = shallowMount(AnswerCard, {
+      propsData: { answer: this.card },
+      stubs: {}
+    })
+  })
+
+  it('renders answer data', function () {
+    expect(this.wrapper.findAll('p').at(1).text()).to.include(this.card.likes)
+  })
+
+  it('renders answer publish time', function () {
+    expect(this.wrapper.find('time').text()).to.include(this.card.published)
+  })
+
+  it('renders answer body', function () {
+    expect(this.wrapper.findAll('p').at(3).text()).to.include(this.card.body)
+  })
+})
---
+++ frontend/tests/unit/AnswerForm.spec.js
@@ -0,0 +1,25 @@
+import { expect } from 'chai'
+import { mount } from '@vue/test-utils'
+import AnswerForm from '@/components/questions-answers/AnswerForm'
+import AppButton from '@/components/AppButton'
+
+describe('AnswerForm.vue', () => {
+  before(function () {
+    this.answer = { body: 'test answer' }
+    this.wrapper = mount(AnswerForm, {
+      propsData: {},
+      stubs: {}
+    })
+    this.wrapper.find('textarea').setValue(this.answer.body)
+    this.wrapper.find(AppButton).trigger('click')
+  })
+
+  it('prop data contains answer body', function () {
+    expect(this.wrapper.vm.$data.body).to.include(this.answer.body)
+  })
+
+  it('button event fired once with correct answer body', function () {
+    expect(this.wrapper.emitted().submitAnswerForm.length).to.equal(1)
+    expect(this.wrapper.emitted().submitAnswerForm[0][0]).to.deep.equal(this.answer)
+  })
+})
