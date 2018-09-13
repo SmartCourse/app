5f5d785297a343b44d4d5b8bd317fe355e5f6ded
Nuno Das Neves <ndasneves@gmail.com>
2018-09-13 18:33:23 +1000

    fixed tests and added new tests for good measure

---
+++ frontend/tests/unit/ReviewCard.spec.js
@@ -0,0 +1,38 @@
+import { expect } from 'chai'
+import { shallowMount, RouterLinkStub } from '@vue/test-utils'
+import ReviewCard from '@/components/reviews-replies/ReviewCard'
+
+describe('ReviewCard.vue', () => {
+  before(function () {
+    this.card = {
+      id: 1,
+      likes: 10,
+      published: new Date().toDateString(),
+      title:
+        'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+      body:
+        'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+    }
+    this.wrapper = shallowMount(ReviewCard, {
+      propsData: { ...this.card },
+      stubs: {
+        'router-link': RouterLinkStub
+      }
+    })
+  })
+  it('renders review data', function () {
+    expect(this.wrapper.findAll('p').at(1).text()).to.include(this.card.likes)
+  })
+
+  it('renders review title', function () {
+    expect(this.wrapper.find('h2').text()).to.include(this.card.title)
+  })
+
+  it('renders review publish time', function () {
+    expect(this.wrapper.find('time').text()).to.equal(this.card.published)
+  })
+
+  it('renders review body', function () {
+    expect(this.wrapper.findAll('p').at(3).text()).to.include(this.card.body)
+  })
+})
