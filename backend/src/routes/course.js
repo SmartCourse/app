4b5c37e2646d14e5b15289f0ea424c43da4eed7c
Alex Hinds <alex.hinds141@gmail.com>
2018-09-13 19:49:20 +1000

    all tests passed bebe

--- backend/src/routes/course.js
+++ backend/src/routes/course.js
@@ -1,23 +1,29 @@
 const express = require('express')
-const router = express.Router()
+const course = express.Router()
 const courseController = require('../controllers/course')
+const questionRouter = require('./question')
+const reviewRouter = require('./review')
+
+/** child routes */
+course.use('/:code/review', reviewRouter)
+course.use('/:code/question', questionRouter)
 
 /* Return all courses in the database */
-router.get('/', courseController.getCourses)
+course.get('/', courseController.getCourses)
 
 /* Get the course data for a specific course id */
-router.get('/:code', courseController.getCourse)
+course.get('/:code', courseController.getCourse)
 
 /* Get page (N) questions for a course */
-router.get('/:code/questions', courseController.getCourseQuestions)
+course.get('/:code/questions', courseController.getCourseQuestions)
 
 /* post a new question to a course page */
-router.post('/:code/question', courseController.postQuestion)
+course.post('/:code/question', courseController.postQuestion)
 
 /* Get page (N) reviews for a course */
-router.get('/:code/reviews', courseController.getCourseReviews)
+course.get('/:code/reviews', courseController.getCourseReviews)
 
 /* create a new review for course */
-router.post('/:code/review', courseController.postReview)
+course.post('/:code/review', courseController.postReview)
 
-module.exports = router
+module.exports = course
--- backend/src/routes/question.js
+++ backend/src/routes/question.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const router = express.Router()
+const question = express.Router({ mergeParams: true })
 const questionController = require('../controllers/question')
 
 /* Get the question data for a specific question id */
-router.get('/:id', questionController.getQuestion)
+question.get('/:id', questionController.getQuestion)
 
 /* Get page (N) answers for a question */
-router.get('/:id/answers', questionController.getQuestionAnswers)
+question.get('/:id/answers', questionController.getQuestionAnswers)
 
 /* Post an answer for a given question */
-router.post('/:id/answers', questionController.postAnswer)
+question.post('/:id/answers', questionController.postAnswer)
 
-module.exports = router
+module.exports = question
