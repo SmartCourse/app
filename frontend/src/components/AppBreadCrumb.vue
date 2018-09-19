5e3e915c331bdb4cc94bc7e4daf929e1d878d313
Nuno Das Neves <ndasneves@gmail.com>
2018-09-19 13:21:25 +1000

    use store to get course name...maybe this will work

--- frontend/src/components/AppBreadCrumb.vue
+++ frontend/src/components/AppBreadCrumb.vue
@@ -8,19 +8,21 @@
 
 <script>
 export default {
-  data() {
-    return {
-        routeList: (() => {
-          // TODO won't work this way... need to have something like:
-          // e.g. Subject > COMP > Management and Ethics > Ask Question
-          // i.e. need to use course store
-          //this.$route.path.split("/")
-          return [
-              {id:0, text:"Subjects", route:"/"},
-              {id:1, text:"COMP", route:"/"},
-              {id:2, text:"Management and Ethics", route:"/"},
-              {id:3, text:"Ask Question", route:"/"}]
-        })()
+  computed: {
+    routeList() {
+      // TODO this
+      // e.g. Subject > COMP > Management and Ethics > Ask Question
+      // i.e. need to use course store
+      //this.$route.path.split("/")
+      const list = []
+      if (this.$route.params.code) {
+          list.push({id:2, text:this.$store.state.course.course.name, route:"/"})
+      }
+      return [
+          {id:0, text:"Subjects", route:"/"},
+          {id:1, text:"COMP", route:"/"},
+          ...list,
+          {id:3, text:"Ask Question", route:"/"}]
     }
   }
 }
