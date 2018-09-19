f5075b58a7a7d474d29ed5a6dd1f9b83c1697a4c
Alex Hinds <alex.hinds141@gmail,com>
2018-09-19 11:07:10 +1000

    possible alternative solution

--- backend/src/models/db/courses.js
+++ backend/src/models/db/courses.js
@@ -2,48 +2,44 @@ const data = require('../../../data/course_data_2019.json')
 const { toLowerCase, decodeUTF8Text } = require('../../utils/helpers')
 
 module.exports = (() => {
+    // unfortunately there are duplicate courses in the data set, so we must filter them
 
-        // unfortunately there are duplicate courses in the data set, so we must filter them
+    // list of courses to return
+    const courses = data.map(subj => {
+        const subjectCode = subj.code
+        const subjectName = decodeUTF8Text(subj.name)
 
-        // list of courses to return
-        const courses = []
+        return subj.courses.map(course => {
+            const studyLevel = course.study_level
 
-        data.forEach(function(subj) {
+            const keywords = decodeUTF8Text(course.keywords)
+            const description = decodeUTF8Text(course.description)
+            const requirements = decodeUTF8Text(course.requirements)
+            const name = decodeUTF8Text(course.name)
 
-            const subjectCode = subj.code;
-            const subjectName = decodeUTF8Text(subj.name);
+            const tags = [
+                course.code,
+                name,
+                subjectCode,
+                subjectName,
+                studyLevel,
+                ...keywords.split(',')]
+                .map(toLowerCase).join(',')
 
-            subj.courses.forEach(function(course) {
-
-                const studyLevel = course.study_level
-
-                const keywords = decodeUTF8Text(course.keywords)
-                const description = decodeUTF8Text(course.description)
-                const requirements = decodeUTF8Text(course.requirements)
-                const name = decodeUTF8Text(course.name)
-
-                const tags = [
-                    course.code,
-                    name,
-                    subjectCode,
-                    subjectName,
-                    studyLevel,
-                    ...keywords.split(',')]
-                    .map(toLowerCase).join(',')
-
-                courses.push({
-                    code: course.code,
-                    name,
-                    studyLevel,
-                    subjectCode,
-                    handbookURL: course.handbook_url,
-                    outlineURL: course.outline_url,
-                    // \n newline isn't parsed by sqlite, so replace it with html
-                    description: description.replace("\n", "<p></p>"),
-                    requirements: requirements.replace("\n", "<p></p>"),
-                    tags
-                })
+            return ({
+                code: course.code,
+                name,
+                studyLevel,
+                subjectCode,
+                handbookURL: course.handbook_url,
+                outlineURL: course.outline_url,
+                // \n newline isn't parsed by sqlite, so replace it with html
+                description: description.replace('\n', '<p></p>'),
+                requirements: requirements.replace('\n', '<p></p>'),
+                tags
             })
         })
-        return courses
-    })()
+    })
+    console.log(courses)
+    return courses
+})()
