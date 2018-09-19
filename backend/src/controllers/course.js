3d29976cb4004d4749d1835f12118d883de97fdf
Travis <travis_b53@hotmail.com>
2018-09-19 17:58:23 +1000

    Backend didn't merge properly before

--- backend/src/controllers/course.js
+++ backend/src/controllers/course.js
@@ -18,24 +18,16 @@ exports.getCourse = function ({ params }, res) {
 
 /* Get all questions for a course */
 exports.getCourseQuestions = function ({ params, query }, res) {
-    // If query 'p' is not given just use p=1
-    let pageNumber = typeof query.p === 'undefined' ? 1 : parseInt(query.p)
-    // Check that pageNumber is an integer
-    if (isNaN(pageNumber)) {
-        throw Error('Page Number is not a number')
-    }
+    let p = parseInt(query.p)
+    const pageNumber = p || 1
     responseHandler(questionModel.getQuestions(params.code, pageNumber), res)
         .catch(errorHandler(res))
 }
 
 /* Get all reviews for a course */
 exports.getCourseReviews = function ({ params, query }, res) {
-    // If query 'p' is not given just use p=1
-    let pageNumber = typeof query.p === 'undefined' ? 1 : parseInt(query.p)
-    // Check that pageNumber is an integer
-    if (isNaN(pageNumber)) {
-        throw Error('Page Number is not a number')
-    }
+    let p = parseInt(query.p)
+    const pageNumber = p || 1
     responseHandler(reviewModel.getReviews(params.code, pageNumber), res)
         .catch(errorHandler(res))
 }
