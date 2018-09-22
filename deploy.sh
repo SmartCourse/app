346fc5f6b7de718c0a4c2cc4caad22fe339fe6be
lp1719 <papandrea.luke@gmail.com>
2018-09-22 12:35:42 +1000

    Ignore commit #1

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,23 @@ cd ../backend
 rm -f smartcourse.zip
 zip -r smartcourse.zip package.json web.config data public src
 
+# Configure environment variables
+cat > tmp_env_vars <<- EOM
+{
+    "WEBSITE_NODE_DEFAULT_VERSION": "8.11.1",
+    "DEPLOYMENT_TYPE": "$type",
+    "FIREBASE_PRIVATE_KEY_ID": "$FIREBASE_PRIVATE_KEY_ID",
+    "FIREBASE_PRIVATE_KEY": "$FIREBASE_PRIVATE_KEY"
+}
+EOM
+
+curl -u $AZURE_USER:$AZURE_PASS \
+    --header "Content-Type: application/json" \
+    --request POST \
+    --data `cat tmp_env_vars` \
+    https://smartcourse-$type.scm.azurewebsites.net/api/settings
+rm tmp_env_vars
+
 # Deploy the site
 curl -u $AZURE_USER:$AZURE_PASS \
     --request POST \
@@ -29,5 +46,5 @@ curl -u $AZURE_USER:$AZURE_PASS \
 curl -u $AZURE_USER:$AZURE_PASS \
     --header "Content-Type: application/json" \
     --request POST \
-    --data '{ "command": "npm install", "dir": "site/wwwroot" }' \
+    --data '{ "command": "rm -rf node_modules; npm install", "dir": "site/wwwroot" }' \
     https://smartcourse-$type.scm.azurewebsites.net/api/command
