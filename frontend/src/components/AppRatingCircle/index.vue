8edce7ffa3670792e9c5ff00b8382d4d94b1287d
Alex Hinds <alex.hinds141@gmail,com>
2018-10-02 15:28:30 +1000

    rating percentage

--- frontend/src/components/AppRatingCircle/index.vue
+++ frontend/src/components/AppRatingCircle/index.vue
@@ -2,9 +2,12 @@
     <svg version="1.1" class="svg-loader" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
      y="0px" viewBox="0 0 80 80" xml:space="preserve">
      <g>
-        <path stroke="grey" stroke-width="5px" fill="none" id="spinner" v-bind:d="backgroundPath"></path>
+        <path stroke="grey" fill="none" v-bind:d="backgroundPath"></path>
         <text text-anchor="middle" alignment-baseline="central" x="40" y="40">{{ percent }}</text>
-        <path stroke="var(--theme)" stroke-width="5px" fill="none" id="spinner" v-bind:d="foregroundPath"></path>
+        <circle v-bind:r="r" :cx="cx" :cy="cy"
+            :transform="`rotate(-90 ${cx} ${cy})`"
+            :stroke-dasharray="circumfrence"
+            :stroke-dashoffset="rating"/>
      </g>
     </svg>
 </template>
@@ -16,7 +19,7 @@ export default {
       cx: 40,
       cy: 40,
       r: 20,
-      percent: 40
+      percent: 80
     }
   },
   computed: {
@@ -24,13 +27,28 @@ export default {
       const { cx, cy, r } = this
       return `M ${cx} ${cy} m -${r}, 0 a ${r} ${r} 0 1 1 ${r * 2},0 a ${r},${r} 0 1,1 -${r * 2},0`
     },
-    foregroundPath() {
-      const { cx, cy, r, radians } = this
-      return `M ${cx} ${cy} m 0, -${r} a ${r} ${r} 0 0 1 ${r + r*Math.cos(radians)},${-r + 2*r*Math.abs(Math.sin(radians))}`
+    circumfrence() {
+      return 2 * Math.PI * this.r
     },
-    radians() {
-      return this.percent / 100 * Math.PI * 2
+    rating() {
+      return ((100 - this.percent) / 100) * this.circumfrence
     }
   }
 }
 </script>
+
+<style scoped>
+path, circle {
+    fill: none;
+    stroke-width: 5px;
+}
+
+text {
+    stroke: var(--black);
+    stroke-width: 0px;
+}
+
+circle {
+    stroke: var(--theme);
+}
+</style>
