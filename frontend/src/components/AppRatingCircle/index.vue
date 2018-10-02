1772ea3a5b5543d8ad5a9ceb6dc199b8e0346cbe
Alex Hinds <alex.hinds141@gmail,com>
2018-10-02 14:53:32 +1000

    can't figure out arcs :(

--- frontend/src/components/AppRatingCircle/index.vue
+++ frontend/src/components/AppRatingCircle/index.vue
@@ -3,8 +3,8 @@
      y="0px" viewBox="0 0 80 80" xml:space="preserve">
      <g>
         <path stroke="grey" stroke-width="5px" fill="none" id="spinner" v-bind:d="backgroundPath"></path>
-        <text text-anchor="middle" alignment-baseline="central" x="40" y="40">10</text>
-        <path stroke="var(--theme)" stroke-width="5px" fill="none" id="spinner" d="M40 20 A 20 20, 0, 0, 1 40 60"></path>
+        <text text-anchor="middle" alignment-baseline="central" x="40" y="40">{{ percent }}</text>
+        <path stroke="var(--theme)" stroke-width="5px" fill="none" id="spinner" v-bind:d="foregroundPath"></path>
      </g>
     </svg>
 </template>
@@ -15,17 +15,21 @@ export default {
     return {
       cx: 40,
       cy: 40,
-      r: 20
+      r: 20,
+      percent: 40
     }
   },
   computed: {
     backgroundPath() {
       const { cx, cy, r } = this
-      return `M ${cx} ${cy} m -${r}, 0 a ${r},${r} 0 1,1 ${r * 2},0 a ${r},${r} 0 1,1 -${r * 2},0`
+      return `M ${cx} ${cy} m -${r}, 0 a ${r} ${r} 0 1 1 ${r * 2},0 a ${r},${r} 0 1,1 -${r * 2},0`
     },
     foregroundPath() {
-      // const { cx, cy, r } = this
-      return 1
+      const { cx, cy, r, radians } = this
+      return `M ${cx} ${cy} m 0, -${r} a ${r} ${r} 0 0 1 ${r + r*Math.cos(radians)},${-r + 2*r*Math.abs(Math.sin(radians))}`
+    },
+    radians() {
+      return this.percent / 100 * Math.PI * 2
     }
   }
 }
