f3787bd125e0dee85f5bb49580546ae779bd2840
Nuno Das Neves <ndasneves@gmail.com>
2018-10-09 11:53:10 +1100

    reverted changes to review card to keep this branch useful

--- frontend/src/components/reviews-replies/ReviewCard.vue
+++ frontend/src/components/reviews-replies/ReviewCard.vue
@@ -8,15 +8,10 @@
             </div>
             <div class="content">
                 <!-- v-if here just stops router error due to async data -->
-                <div :class="'recommend '+recommendIcon.class">
-                    <i class="material-icons">{{ recommendIcon.icon }}</i>
-                </div>
                 <router-link v-if="code" tag="h2" :to="{ name: 'review', params: { code, id }}">
                     {{ title }}
                 </router-link>
-                <p>
-                    {{ body }}
-                </p>
+                <p>{{ body }}</p>
             </div>
             <aside class="user">
                 <User :image="'https://travis-ci.com/images/logos/TravisCI-Mascot-1.png'"/>
@@ -35,43 +30,14 @@ export default {
   props: {
     code: String,
     id: String,
+    author: Number,
+    likes: Number,
     title: String,
     body: String,
-    likes: Number,
-    recommend: Number,
-    author: Number,
     published: String
-  },
-  computed: {
-    recommendIcon () {
-      if (this.recommend === 1) {
-        return {class: 'blue', icon: 'thumb_up', text: 'recommended'}
-      } else {
-        return {class: 'red', icon: 'thumb_down', text: 'not recommended'}
-      }
-    }
   }
 }
 </script>
 
 <style scoped src='../../css/card.less' lang='less'>
-</style>
-
-<style scoped>
-.material-icons {
-    font-size:35px;
-}
-h2 {
-    margin-top:3px;
-}
-.recommend {
-    float:left;
-    margin-right:10px;
-}
-.blue {
-    color:#0099ee;
-}
-.red {
-    color:#dd2222;
-}
 </style>
