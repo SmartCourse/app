9051671ed9f3a58bdd1b7c4ab02a5ec87cd2b3f9
Travis <travis_b53@hotmail.com>
2018-10-17 02:43:55 +1100

    Fixed import

--- frontend/src/utils/api/reviews.js
+++ frontend/src/utils/api/reviews.js
@@ -1,5 +1,5 @@
 import { get, post, put } from './index'
-import { formatDistanceStrict } from 'date-fns'
+import formatDistanceStrict from 'date-fns'
 
 /* get review */
 export function getReview(course, id) {
