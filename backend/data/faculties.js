237561152e940509ca20d8b1d8c792b90592eb63
Alex Hinds <alex.hinds141@gmail.com>
2018-10-18 12:39:12 +1100

    Degree data

---
+++ backend/data/faculties.js
@@ -0,0 +1,48 @@
+
+module.exports = [
+    {
+        node: {
+            title: 'Art & Design', slug: 'art-design'
+        }
+    },
+    {
+        node: {
+            title: 'Arts & Social Sciences', slug: 'arts-social-sciences'
+        }
+    },
+    {
+        node: {
+            title: 'Built Environment', slug: 'built-environment'
+        }
+    },
+    {
+        node: {
+            title: 'Business School', slug: 'business-school'
+        }
+    },
+    {
+        node: {
+            title: 'Engineering', slug: 'engineering'
+        }
+    },
+    {
+        node: {
+            title: 'Law', slug: 'law'
+        }
+    },
+    {
+        node: {
+            title: 'Medicine', slug: 'medicine'
+        }
+    },
+    {
+        node: {
+            title: 'Science', slug: 'science'
+        }
+    },
+    {
+        node: {
+            title: 'UNSW Canberra', slug: 'unsw-canberra'
+        }
+    }
+]
