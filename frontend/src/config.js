7f2b62506fc3a90fd064ed2d3d36501aaffd1c14
Alex Hinds <alex.hinds141@gmail,com>
2019-01-17 16:44:40 +1100

    removed prod devtools

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -8,11 +8,6 @@ import AppBreadCrumb from '@/components/AppBreadCrumb'
 // import VTooltip from 'v-tooltip'
 // Vue.use(VTooltip)
 
-Vue.config.productionTip = false
-
-// TODO remove me
-Vue.config.devtools = true
-
 Vue.component('LoadingSpinner', LoadingSpinner)
 Vue.component('AppLogo', AppLogo)
 Vue.component('AppBreadCrumb', AppBreadCrumb)
