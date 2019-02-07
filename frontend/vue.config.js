3b0c2f96bbf2f135ad39dab8635e04e1e2ac2a16
Alex Hinds <alex.hinds141@gmail,com>
2019-02-07 15:43:11 +1100

    postProcess so data-server-rendeered included

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,9 +2,11 @@ const path = require('path')
 const PrerenderSpaPlugin = require('prerender-spa-plugin')
 const PRE_RENDERED_ROUTES = require('../backend/pre-rendered')
 
+const distDir = path.join(__dirname, '../backend/public')
+
 const productionPlugins = [
   new PrerenderSpaPlugin({
-    staticDir: path.join(__dirname, '../backend/public'),
+    staticDir: distDir,
     routes: PRE_RENDERED_ROUTES,
     renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
       // We need to inject a value so we're able to
@@ -15,9 +17,25 @@ const productionPlugins = [
       // so we create a snapshot of the page after the
       // `data-view` attribute exists in the DOM.
       renderAfterElementExists: '[data-view]'
-    })
+    }),
+    postProcess (renderedRoute) {
+      // Ignore any redirects.
+      renderedRoute.route = renderedRoute.originalRoute
+      // Basic whitespace removal. (Don't use this in production.)
+      renderedRoute.html = renderedRoute.html.split(/<div id="app"/)
+        .join('<div id="app" data-server-rendered="true"')
+      // Remove /index.html from the output path if the dir name ends with a .html file extension.
+      // For example: /dist/dir/special.html/index.html -> /dist/dir/special.html
+      if (renderedRoute.route.endsWith('.html')) {
+        renderedRoute.outputPath = path.join(__dirname, 'dist', renderedRoute.route)
+      }
+
+      return renderedRoute
+    },
   })
 ]
+
+
 
 module.exports = {
   lintOnSave: true,
