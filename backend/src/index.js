dd73109ec46f60638acdad4ca79eb4494b57f8fc
Alex Hinds <alex.hinds141@gmail.com>
2019-02-08 16:01:18 +1100

    Init

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,6 @@ const logger = require('morgan')
 const firebase = require('./auth')
 const compression = require('compression')
 const db = require('./models/db')
-const PRE_RENDERED_TEMPLATES = require('../pre-rendered')
 const { APIErrorHandler } = require('./error')
 const { staticFilesCache } = require('./utils/cache')
 const { PRODUCTION } = require('./models/constants')
@@ -44,17 +43,6 @@ app.use('/api', apiRouter)
 app.use(staticFilesCache)
 
 /*
- * These templates are prerendered to enchance SEO.
- * If requests are returned for these routes, return the template.
- */
-PRE_RENDERED_TEMPLATES
-    .forEach(route => {
-        app.use(`${route}`, function (_, res) {
-            res.sendFile(path.join(__dirname, `../public${route}`, 'index.html'))
-        })
-    })
-
-/*
     anything that gets here and not handled
     by api error handler should get index.html.
     NB: frontend must handle actual 404s.
@@ -71,6 +59,6 @@ app.use(APIErrorHandler)
 db.on('ready', () => {
     console.log('App ready!')
     app.emit('ready')
-  })
+})
 
 module.exports = app
