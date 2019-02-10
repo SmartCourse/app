644d3ecf37c6be6ba06a1b2942aa342c689c8c93
Nuno Das Neves <ndasneves@gmail.com>
2019-02-10 17:14:34 +1100

    removed pre-rendered.js

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,7 @@ npm run build-$type
 cd ../backend
 rm -f smartcourse.zip
 cp scripts/backup.sh .
-zip -r smartcourse.zip pre-rendered.js package.json package-lock.json web.config backup.sh data public src scripts
+zip -r smartcourse.zip package.json package-lock.json web.config backup.sh data public src scripts
 rm backup.sh
 echo ""
 
