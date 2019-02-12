b940d798acf9d25f1969d88873d5a112b22c2bbc
Nuno Das Neves <ndasneves@gmail.com>
2019-02-12 20:03:15 +1100

    Are we frontend yet (#360)
    * moved backend files to SmartCourse/api repo
    
    * deploy now just builds app and pushes to ./public folder on azure

--- backend/data/subjects.js
+++
@@ -1,9 +0,0 @@
-const subjects = require('./course_data_2019.json')
-const { decodeUTF8Text } = require('../src/utils/helpers')
-
-module.exports = subjects.map(({ name, code, url }) => ({
-    code,
-    name: decodeUTF8Text(name),
-    handbookURL: url,
-    universityID: 1
-}))
